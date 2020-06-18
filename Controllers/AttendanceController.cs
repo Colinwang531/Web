93015c67b54490015191e49f9190583ce1ac62f0
zhuzhirong0423 <494684778@qq.com>
2020-06-18 15:18:31 +0800

    修改考勤页面

--- Controllers/AttendanceController.cs
+++ Controllers/AttendanceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.Util;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CodeStyle;
 using ShipWeb.DB;
@@ -26,7 +27,7 @@ namespace ShipWeb.Controllers
         /// </summary>
         /// <param name="type">1：考勤入 2：考勤出</param>
         /// <returns></returns>
-        public IActionResult Load(int type=1)
+        public IActionResult Load(int pageIndex,int pageSize)
         {
             try
             {
@@ -34,7 +35,6 @@ namespace ShipWeb.Controllers
                            join b in _context.AlarmInformation on a.Id equals b.AlarmId
                            join c in _context.CameraConfig on b.Cid equals c.Cid
                            join d in _context.Employee on b.Uid equals d.Uid
-                           where (type == 1 ? c.EnableAttendanceIn == true : c.EnableAttendanceOut == true)
                            select new
                            {
                                d.Uid,
@@ -45,13 +45,90 @@ namespace ShipWeb.Controllers
                                Picture = Convert.ToBase64String(Convert.FromBase64String(Encoding.UTF8.GetString(a.Picture))),
                            };
                 var list = data.ToList();
-                return new JsonResult(new { code = 0, data = list });
+                int count = list.Count;
+                var pageList=list.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+                var result = new
+                {
+                    code = 0,
+                    data = pageList,
+                    pageIndex = pageIndex,
+                    pageSize = pageSize,
+                    count = count
+                };
+                return new JsonResult(result);
             }
             catch (Exception ex)
             {
                 return new JsonResult(new { code = 1, msg = "获取数据失败!"+ex.Message });
             }
 
+        }
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="uid">编号</param>
+        /// <param name="name">名称</param>
+        /// <param name="pageIndex">当前页</param>
+        /// <param name="pageSize">每次显示条数</param>
+        /// <returns></returns>
+        public IActionResult QueryPage(string uid, string name, string startTime,string endTime, int pageIndex, int pageSize)
+        {
+            try
+            {
+                var data = from a in _context.Alarm
+                           join b in _context.AlarmInformation on a.Id equals b.AlarmId
+                           join c in _context.CameraConfig on b.Cid equals c.Cid
+                           join d in _context.Employee on b.Uid equals d.Uid
+                           select new
+                           {
+                               d.Uid,
+                               d.Name,
+                               a.Time,
+                               c.EnableAttendanceIn,
+                               c.EnableAttendanceOut,
+                               Picture = Convert.ToBase64String(Convert.FromBase64String(Encoding.UTF8.GetString(a.Picture))),
+                           };
+                if (!string.IsNullOrEmpty(uid))
+                {
+                    data=data.Where(c => c.Uid == uid);
+                }
+                if (!string.IsNullOrEmpty(name))
+                {
+                    data=data.Where(c => c.Name.Contains(name));
+                }
+                if (!(string.IsNullOrEmpty(startTime))&&!(string.IsNullOrEmpty(endTime)))
+                {
+                    DateTime dtStart = DateTime.Parse(startTime);
+                    DateTime dtEnd = DateTime.Parse(endTime);
+                    data = data.Where(c => c.Time >= dtStart && c.Time <= dtEnd);
+                }
+                else if (!(string.IsNullOrEmpty(startTime)))
+                {
+                    DateTime dtStart = DateTime.Parse(startTime);
+                    data = data.Where(c => c.Time >= dtStart );
+                }
+                else if (!(string.IsNullOrEmpty(endTime)))
+                {
+                    DateTime dtEnd = DateTime.Parse(endTime);
+                    data = data.Where(c => c.Time <= dtEnd);
+                }
+                var list = data.ToList();
+                int count = list.Count();
+                var pageList = list.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+                var result = new
+                {
+                    code = 0,
+                    data = pageList,
+                    pageIndex = pageIndex,
+                    pageSize = pageSize,
+                    count = count
+                };
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { code = 1, msg = "查询失败!"+ex.Message });
+            }
         }
     }
 }
